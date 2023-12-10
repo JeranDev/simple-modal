46b902fef2be51b1f43bbad456c7710607879ee2
jeran <jeranb@protonmail.com>
2023-12-10 15:04:32 -0700

    add closeModal function

--- script.js
+++ script.js
@@ -19,13 +19,12 @@ openModalBtn.addEventListener('click', () => {
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
-closeModalBtn.addEventListener('click', () => {
-  modal.classList.remove('open')
-  overlay.classList.remove('open')
-})
+closeModalBtn.addEventListener('click', closeModal)
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener('click', () => {
+overlay.addEventListener('click', closeModal)
+
+function closeModal() {
   modal.classList.remove('open')
   overlay.classList.remove('open')
-})
+}
